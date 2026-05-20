686b8d0651aa76b7503ede63c09fe875277f4ddd
manishtope <manishtope@gmail.com>
2026-05-20 14:07:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import './App.css';
 import SignalDashboard from './pages/SignalDashboard';
 
 function App() {
   return (
-    <div className="App dark min-h-screen bg-[#050505] text-white">
-      <SignalDashboard />
-    </div>
+    <SignalDashboard />
   );
 }
 
